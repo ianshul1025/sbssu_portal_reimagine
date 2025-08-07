914a523f5e99ff5f439cf02427fc45f5ea1f3f64
Bharat Pandav <developerbharat01@gmail.com>
2025-08-07 07:56:40 -0700

    Create LeaderShipData.jsx

---
+++ src/pages/About/LeaderShip/LeaderShipData.jsx
@@ -0,0 +1,15 @@
+const LeaderShipData = [
+  {
+    id: "leadership-Chancellor", // Remove leading slash for consistency
+    title: "Chancellor",
+    image: "", // Add image URL if available
+    paragraphs: ["write data here"], // Make this an array
+  },
+  {
+    id: "leadership-chairman", 
+    title: "chairman",
+    image: "", 
+    paragraphs: ["write data here"],
+  }
+];
+export default LeaderShipData;
